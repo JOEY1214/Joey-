60a9526276b91d991c8202dc6e531796af5d2ea2
罗雨涵 <luoyuhan@luoyuhandemacbook-air.local>
2018-01-08 18:39:05 +1100

    First project and start

---
+++ Joey_live/Joey_live/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Joey_live
+//
+//  Created by 罗雨涵 on 8/1/18.
+//  Copyright © 2018 罗雨涵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
