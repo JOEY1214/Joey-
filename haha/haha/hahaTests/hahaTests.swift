b80f839447af9aa39c6394b17a3505f27bd973af
罗雨涵 <luoyuhan@luoyuhandemacbook-air.local>
2018-01-10 16:36:04 +1100

    event

---
+++ haha/haha/hahaTests/hahaTests.swift
@@ -0,0 +1,36 @@
+//
+//  hahaTests.swift
+//  hahaTests
+//
+//  Created by 罗雨涵 on 10/1/18.
+//  Copyright © 2018 罗雨涵. All rights reserved.
+//
+
+import XCTest
+@testable import haha
+
+class hahaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
